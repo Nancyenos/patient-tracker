e21c6b3d12e584df2203f6ab877152d80418c14c
nancyenos <nicymimz@gmail.com>
2024-09-24 13:50:09 -0700

    Set up Flask-Login for user authentication

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from routes import api
+from flask_login import LoginManager
+from models import db, User
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -10,7 +12,11 @@ app.config.from_object(Config)
 # Initialize the database and migration
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+login_manager = LoginManager(app)
 
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 # Register the API routes from routes.py
 app.register_blueprint(api)
 
